2c6f7c90224e04aac5a73b7a2aa0933122aed967
cjuhal <chrisjuhal@gmail.com>
2017-12-30 00:12:23 -0300

    Install files fixed
    version 2
    Se utiliza la funcion de php copy para sobreescribir si ya existe, o crear en caso de no existir.
    
    Por ahora al desinstalar no los borramos.
    
    Falta generarlo por array

--- wp-content/plugins/suplementos/suplementos.php
+++ wp-content/plugins/suplementos/suplementos.php
@@ -14,25 +14,25 @@ function init() {
 
 	$file = plugin_dir_path(__FILE__). '/export/suplemento-view.php';
 	$destino = get_template_directory() . '/template-parts/suplemento-view.php';
-   if (!rename($file, $destino)) {
+   if (!copy($file, $destino)) {
    	echo "<script><alert>Error al copiar $file...\n</alert></script>";
 	}
 
 	$file = plugin_dir_path(__FILE__). '/export/content-single-suplemento.php';
 	$destino = get_template_directory() . '/template-parts/content-single-suplemento.php';
-	 if (!rename($file, $destino)) {
+	 if (!copy($file, $destino)) {
 		echo "<script><alert>Error al copiar $file...\n</alert></script>";
 	}
 
 	$file = plugin_dir_path(__FILE__). '/export/single-suplemento.php';
 	$destino = get_template_directory() . '/single-suplemento.php';
-	 if (!rename($file, $destino)) {
+	 if (!copy($file, $destino)) {
 		echo "<script><alert>Error al copiar $file...\n</alert></script>";
 	}
 
 	$file = plugin_dir_path(__FILE__). '/export/archive-suplemento.php';
 	$destino = get_template_directory() . '/archive-suplemento.php';
-	 if (!rename($file, $destino)) {
+	 if (!copy($file, $destino)) {
 		echo "<script><alert>Error al copiar $file...\n</alert></script>";
 	}
 
